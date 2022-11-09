13c3a894e02abfaf320be61c2987ca82b51d8e02
vikram <vk14133m02059@gmail.com>
2022-11-09 22:43:58 +0530

    Added Day-2 task

---
+++ src/Components/Navbar.jsx
@@ -0,0 +1,13 @@
+function Navbar(){
+    return(
+        <div>
+            <img src="RiViK Tv +" alt="" />
+            <div>
+                <input type="text" placeholder="Search" />
+                <button>Sign in</button>
+            </div>
+        </div>
+    );
+}
+
+export default Navbar;
