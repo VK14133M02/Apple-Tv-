5b81ffc6dad1b86edcd3be513b589fcacad8ecae
vikram <vk14133m02059@gmail.com>
2022-11-12 20:52:24 +0530

    Added my Project

--- src/App.js
+++ src/App.js
@@ -1,15 +1,17 @@
-import logo from './logo.svg';
-// import './App.css';
-import BackgroundImage from "./Components/BackgroundImage";
-import Navbar from './Components/Navbar';
+
+import './App.css';
+import { AllRoutes } from './componants/AllRoutes/AllRoutes';
+
+import Navbar from './componants/Home/Navbar/Navbar';
+
 
 function App() {
   return (
     <div className="App">
       <Navbar />
-      <BackgroundImage />
+      <AllRoutes />
     </div>
   );
 }
-
+//https://github.com/vadimghedreutan/Apple-tv-plus-clone/tree/main/src
 export default App;
